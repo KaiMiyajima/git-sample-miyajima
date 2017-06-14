c5b326f1793c00163c599a23fb0c54647f4381fc
yajimayosuke <yosuke.yajima@rakus.co.jp>
2017-06-14 11:33:08 +0900

    テストクラスを作成しました

---
+++ src/main/java/com/example/demo/Test.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+public class Test {
+	private Integer point;
+	private String name;
+	public Integer getPoint() {
+		return point;
+	}
+	public void setPoint(Integer point) {
+		this.point = point;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
